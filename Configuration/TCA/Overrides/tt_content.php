57db48b624a41b4ba04cb36c7d248fa5d58597ad
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2024-09-27 10:12:02 +0200

    Update tt_content.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3') || die();
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
@@ -31,3 +29,37 @@ if (!defined('TYPO3_MODE')) {
 ]);
 
 
+
+call_user_func(
+    function ($extKey) {
+        $extKey = "ws_flexslider";
+        // Build extension name vars - used for plugin registration, flexforms and similar
+        $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey);
+        $pluginSignature = strtolower($extensionName) . '_pi1';
+        
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi1',
+            'WS Flexslider'
+            );
+        
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript',
+            'Flexslider');
+        
+        
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wsflexslider_domain_model_image');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tx_wsflexslider_domain_model_image',
+            'EXT:ws_flexslider/Resources/Private/Language/locallang_csh_image.xlf');
+        
+        
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'tx_wsflexslider_images,pi_flexform';
+        
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
+            'FILE:EXT:' . $extKey . '/Configuration/FlexForm/Flexslider.xml');
+        
+        
+    },
+    'ws_flexslider'
+    );
