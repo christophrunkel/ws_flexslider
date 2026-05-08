f75577bbc98599407611a57c0676d6021a64c1c0
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2026-05-08 09:22:48 +0200

    Update fort Typo3 13.4

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -47,10 +47,7 @@ call_user_func(
             'Flexslider');
         
         
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wsflexslider_domain_model_image');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-            'tx_wsflexslider_domain_model_image',
-            'EXT:ws_flexslider/Resources/Private/Language/locallang_csh_image.xlf');
+ 
         
         
         $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
